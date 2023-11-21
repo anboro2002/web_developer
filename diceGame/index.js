180b2dbbde3d260070d6a9430670a8afcd1ab91d
anboro2002 <131259120+anboro2002@users.noreply.github.com>
2023-11-21 16:56:00 +0200

    Add files via upload

---
+++ diceGame/index.js
@@ -0,0 +1,29 @@
+/* random dice */
+
+var randomNumber1 = Math.floor(Math.random() * 6) +1;
+var randomNumber2 = Math.floor(Math.random() * 6) +1;
+
+var randomDiceImage1 = "./images/dice"+randomNumber1+".png";
+var randomDiceImage2 = "./images/dice"+randomNumber2+".png";
+
+document.querySelector(".img1").setAttribute("src",randomDiceImage1);
+document.querySelector(".img2").setAttribute("src",randomDiceImage2);
+
+
+/* Win / Draw message */
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = ("🚩 Player 1 Wins!");
+} else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = ("Player 2 Wins! 🚩");
+} else if(randomNumber1 === randomNumber2){
+    document.querySelector("h1").innerHTML = ("Draw");
+}
+
+
+/* Play button (refresh page) */
+
+document.querySelector(".btn").addEventListener("click", function(){
+    location.reload();
+});
+
