94ac92cffdfb63648991b5a57cda7fbd8b77a216
anboro2002 <131259120+anboro2002@users.noreply.github.com>
2023-11-21 17:02:09 +0200

    Update index.js

--- diceGame/index.js
+++ diceGame/index.js
@@ -1,3 +1,8 @@
+/* names */
+
+var playerName1 = Prompt("Player 1:");
+var playerName2 = Prompt("Player 2:");
+
 /* random dice */
 
 var randomNumber1 = Math.floor(Math.random() * 6) +1;
@@ -13,9 +18,9 @@ document.querySelector(".img2").setAttribute("src",randomDiceImage2);
 /* Win / Draw message */
 
 if(randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerHTML = ("🚩 Player 1 Wins!");
+    document.querySelector("h1").innerHTML = "🚩 "+playerName1+" Wins!";
 } else if(randomNumber2 > randomNumber1){
-    document.querySelector("h1").innerHTML = ("Player 2 Wins! 🚩");
+    document.querySelector("h1").innerHTML = playerName2 + " Wins! 🚩";
 } else if(randomNumber1 === randomNumber2){
     document.querySelector("h1").innerHTML = ("Draw");
 }
